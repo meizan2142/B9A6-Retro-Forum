ee29408f68255c333b4cb915392954bee7f76b1d
Your Name01~git <saifsahai2104@gmail.com>
2024-03-05 11:11:10 +0600

    Latest post's functionality completed

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const displayData = (everyPosts) => {
     const discussSection = document.getElementById('discuss-card');
     // console.log(everyPosts);
     everyPosts.forEach(everyPost => {
-        console.log(everyPost);
+        // console.log(everyPost);
         const discussCard = document.createElement('div');
         discussCard.classList = `left-side lg:w-[772px] lg:h-[220px] bg-[#F3F3F5] rounded-xl grid grid-cols-1 lg:flex gap-6 p-8 lg:p-6`;
         discussCard.innerHTML = `
@@ -53,8 +53,9 @@ const displayData = (everyPosts) => {
         `
         discussSection.appendChild(discussCard);
     });
-
+    
 }
+
 
 
 let count = 0;
@@ -81,72 +82,83 @@ const message = (title,view_count) => {
     container.appendChild(newContainer);
 }
 
-
 allPosts();
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const searchBtn = async (searchText) => {
-//     // console.log('I am clicked');
-//     const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/postshttps://openapi.programming-hero.com/api/retro-forum/posts?category=${category.name}`);
-//     const category = await response.json();
-//     const post = category.posts;
-//     const inputField = document.getElementById('input-text');
-//     const search = inputField.value;
-//     console.log(search, post);
-// }
-// // seachBtn(search);
-
-const latestPosts = async () => {
+const loadLatestPost =async () => {
     const response = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
     const data = await response.json();
-    const post = data;
-    console.log(post);
+    const cardContainer = document.getElementById('card-container');
+    data.forEach((item) => {
+    // console.log(item);
+    const divOfCard = document.createElement('div');
+    divOfCard.classList = `card lg:w-96 bg-base-100 shadow-xl p-2 lg:p-0`
+    divOfCard.innerHTML = `
+    <figure class="lg:px-10 lg:pt-10">
+    <div class="w-[230px] lg:w-[326px] h-[150px] lg:h-[190px] grid bg-[#eeeef0] rounded-lg">
+        <img src="${item.cover_image}" alt="">
+    </div>
+</figure>
+<div class="card-body">
+    <div class="flex gap-2">
+        <img src="icon/download (5).png" alt="">
+        <p class="text-gray-400">${item.author.posted_date}</p>
+    </div>
+    <div>
+        <p class="text-black font-bold w-[221px]">What will a mars habitat force that impact in our
+            daily life!!!</p>
+        <p class="w-[209px] text-gray-400">Yes, you can run unit tests and view the results directly
+            within the app.</p>
+    </div>
+    <div class="flex gap-4">
+        <img class="w-[40px] h-[40px] rounded-full" src="${item.profile_image}" alt="">
+        <div>
+            <p class="font-bold ">${item.author.name}</p>
+            <p class="text-gray-400">${item.author.designation}</p>
+        </div>
+    </div>
+</div>
+    `
+    cardContainer.appendChild(divOfCard);
+    })
 }
 
-// const displayData2 = () => {
-//     const discussSection = document.getElementById('discuss-card');
-//     // console.log(everyPosts);
-//     .forEach() => {
-//         console.log(everyPost);
-// }
 
-// const displayData2 = async () => {
-//     const 
-// }
+const loadCategory =async () => {
+    const inputField = document.getElementById('input-text');
+    const searchText = inputField.value;
+    console.log(searchText);
+    const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${name}`);
+    const data = await response.json();
+    console.log(data.posts.category);
+    // console.log('I am clicked');
+    // data.posts.forEach((cate) => {
+    //     console.log(cate.category);
+    // })
+}
 
-latestPosts();
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+loadLatestPost();
